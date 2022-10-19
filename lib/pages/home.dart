a4b93976d613830b33fadea19f3fd97a92d32c58
Ivan Rybets <ivan.rybets@onix-systems.com>
2022-10-19 23:22:09 +0300

    fix null safe module

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -211,6 +211,7 @@ class _HomeState extends State<Home> {
                     icon: getPlayModeIcon(playMode),
                     onPressed: () {
                       playMode = AudioManager.instance.nextMode();
+
                       setState(() {});
                     }),
                 Row(children: [
